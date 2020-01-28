366d8bdd07b58a99b3d797ff100a4530cb82e111
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-28 16:27:27 +0000

    Adds tests for Library class

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library(5);
+        book = new Book("The Handmaid's Tale", "Margaret Attwood", "Sci-Fi");
+    }
+
+    @Test
+    public void libraryIsEmptyToStart(){
+        assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBookToStock(){
+        library.addBook(book);
+        assertEquals(1, library.countBooks());
+    }
+
+    @Test
+    public void checkLibraryHasSpace() {
+        library.addBook(book);
+        library.addBook(book);
+        assertTrue(library.checkCapacity());
+    }
 }
