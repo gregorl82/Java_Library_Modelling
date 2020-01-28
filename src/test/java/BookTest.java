030f049ae1477553807867ed3528777518b1531f
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-28 16:16:56 +0000

    Adds tests for Book class properties

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before(){
+        book = new Book("The Great Gatsby", "F. Scott Fitzgerald", "Classics");
+    }
+
+    @Test
+    public void hasATitle(){
+        assertEquals("The Great Gatsby", book.getTitle());
+    }
+
+    @Test
+    public void hasAnAuthor(){
+        assertEquals("F. Scott Fitzgerald", book.getAuthor());
+    }
+
+    @Test
+    public void hasAGenre(){
+        assertEquals("Classics", book.getGenre());
+    }
+
 }
