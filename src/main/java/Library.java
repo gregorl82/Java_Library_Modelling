f99daf1aa569635cb557294ac34cfdf6136c18cf
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-28 20:21:35 +0000

    Adds comments to numberOfBooksByGenre method for clarity

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -30,16 +30,26 @@ public class Library {
     }
 
     public int numberOfBooksByGenre(String genre) {
+
+        // Initialise a new HashMap for the number of books of each genre
         HashMap<String, Integer> genreHash = new HashMap<>();
+
+        // Loop through each of the books in the library's stock
         for (Book book : this.stock) {
             String bookGenre = book.getGenre();
+
+            // if the book's genre is already in the HashMap, increase the associated value by 1
             if (genreHash.containsKey(bookGenre)){
                 int newValue = genreHash.get(bookGenre) + 1;
                 genreHash.put(bookGenre, newValue);
+
+            // otherwise, create a new key with an initial value of 1
             } else {
                 genreHash.put(bookGenre, 1);
             }
         }
+
+        // return the value associated with the passed in genre
         return genreHash.get(genre);
     }
 }
