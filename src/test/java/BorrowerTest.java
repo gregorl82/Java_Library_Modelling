5be46ef7dfdd557f9963ce169f3f5b00ebe9015e
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-28 18:11:27 +0000

    Creates tests for Borrower class

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    private Borrower borrower;
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        borrower = new Borrower("George");
+        library = new Library(12);
+        book = new Book("Pride and Prejudice", "Jane Austen", "Classics");
+    }
+
+    @Test
+    public void hasAName(){
+        assertEquals("George", borrower.getName());
+    }
+
+    @Test
+    public void collectionIsEmptyToStart(){
+        assertEquals(0, borrower.countBooks());
+    }
+
+    @Test
+    public void canBorrowABook(){
+        borrower.borrowBook(library);
+        assertEquals(1, borrower.countBooks());
+    }
+
 }
