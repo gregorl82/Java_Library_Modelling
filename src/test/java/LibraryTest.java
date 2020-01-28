635504c47ac0b06e6d4773d65d9cb59b4d934507
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-28 19:43:47 +0000

    Adds test for a numberOfBooksByGenre method

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,12 +6,22 @@ import static org.junit.Assert.*;
 public class LibraryTest {
 
     private Library library;
-    private Book book;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Book book4;
+    private Book book5;
+    private Book book6;
 
     @Before
     public void before(){
         library = new Library(5);
-        book = new Book("The Handmaid's Tale", "Margaret Attwood", "Sci-Fi");
+        book1 = new Book("The Handmaid's Tale", "Margaret Attwood", "Sci-Fi");
+        book2 = new Book("2001: A Space Odyssey", "Arthur C. Clarke", "Sci-Fi");
+        book3 = new Book("Wuthering Heights", "Emily Bronte", "Classics");
+        book4 = new Book("Game of Thrones", "George R.R. Martin", "Fantasy");
+        book5 = new Book("The Grapes of Wrath", "John Steinbeck", "Classics");
+        book6 = new Book("A Walk in the Woods", "Bill Bryson", "Travel");
     }
 
     @Test
@@ -21,37 +31,47 @@ public class LibraryTest {
 
     @Test
     public void canAddBookToStock(){
-        library.addBook(book);
+        library.addBook(book1);
         assertEquals(1, library.countBooks());
     }
 
     @Test
     public void checkLibraryHasSpace() {
-        library.addBook(book);
-        library.addBook(book);
+        library.addBook(book1);
+        library.addBook(book2);
         assertTrue(library.checkCapacity());
     }
 
     @Test
     public void cannotAddBookToStockIfNoCapacity(){
-        library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        library.addBook(book5);
+        library.addBook(book6);
         assertEquals(5, library.countBooks());
         assertFalse(library.checkCapacity());
     }
 
     @Test
     public void canRemoveABook(){
-        library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
         Book removedBook = library.removeBook();
         assertEquals(3, library.countBooks());
         assertNotNull(removedBook);
+    }
+
+    @Test
+    public void canGetNumberOfBooksByGenre(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        library.addBook(book5);
+        assertEquals(2, library.booksByGenre("Sci-Fi"));
     }
 }
